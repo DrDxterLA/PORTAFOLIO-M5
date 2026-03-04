e27fd7738a0c07ad1943d52d5b849285ff9e010d
DrDxterLA <cristobal.riffo.berna@gmail.com>
2026-03-04 19:30:41 -0300

    feat: crear archivo de datos para proyectos

---
+++ data/projects.js
@@ -0,0 +1,22 @@
+const projects = [
+  {
+    id: 1,
+    title: "RiBer Eventos",
+    category: "Eventos",
+    description: "Producción de bodas y eventos empresariales"
+  },
+  {
+    id: 2,
+    title: "Galería Fotográfica",
+    category: "Desarrollo Web",
+    description: "Sistema privado para entrega de fotografías"
+  },
+  {
+    id: 3,
+    title: "ERP PYME",
+    category: "Sistema",
+    description: "Gestión gastronómica completa"
+  }
+];
+
+export default projects;
